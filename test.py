a01c0702bc7de36b5aab032c9e4c8d3973cab08d
CathrineUH <s191780@student.dtu.dk>
2022-02-04 11:38:06 +0100

    jd

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from color_dtu_design.color_design import color_design as col
 import matplotlib.pyplot as plt
 
+
+
 # x axis values
 x = [1,2,3,4,5,6]
 # corresponding y axis values
@@ -8,8 +10,10 @@ y = [2,4,1,5,2,6]
 
 
 z = col("pink").color
-z = col("pink").color
+
+
 # plotting the points
+plt.figure
 plt.plot(x, y, color = z, linestyle='dashed', linewidth = 3,
 		marker='o', markerfacecolor=z, markersize=12)
  
