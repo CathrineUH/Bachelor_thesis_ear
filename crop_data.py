1c0a195f8670c81f8073c49ea363815e8b6b2cfa
CathrineUH <97507818+CathrineUH@users.noreply.github.com>
2022-02-07 14:49:25 +0100

    jj

--- crop_data.py
+++ crop_data.py
@@ -1,18 +1,40 @@
-from Dataloader import loadScan
-import itk
-import numpy as np
-path = "C:\\Users\\There\\OneDrive\\Uddannelse\\Universitet\\6 semester\\Bachelorprojekt\\Data"
-savePath = "C:\\Users\\There\\OneDrive\\Dokumenter\\GitHub\\Bachelor-thesis-ear\\Data_cropped"
+from Dataloader import loadScan as ls
+import SimpleITK as sitk
+def resampleImage(image):
+    '''
 
-for i in range(1, 109+1): 
-    if i == 16 or i == 78: 
-        continue 
-    im = loadScan(path, i)
-    if im.shape == (160, 512, 392): 
-        im_save = im[:, 230:330, 220:320]
-    elif im.shape == (128, 256, 192): 
-        im_save = im[:, 90:190, 80:180]
-    
-    if len(str(i)) == 1: 
+    '''
+    im =  ls('Data',1)
+    if(image.GetSize() == im.GetSize()):
+        return image
+    else:
+        im_temp = sitk.Resample(image,im)
+    return im_temp
+
+def getEars(image):
+    return image[225:325,70:170,:],image[230:330,220:320,:]
+
+def saveCropImage(path):
+    for i in range(1,(109+1)):
+        if i == 16 or i == 78:
+            continue
+        im = resampleImage(ls(path,i))
+        im_1,im_2 = getEars(im)
+        i = str(i)
+        if len(i) == 1:
+            sitk.WriteImage(im_1, "Data_cropped\\ear1-00"+ i +".mha")
+            sitk.WriteImage(im_2, "Data_cropped\\ear1-00"+ i +".mha")
+        elif len(i) == 2:
+            sitk.WriteImage(im_1, "Data_cropped\\ear1-0"+ i +".mha")
+            sitk.WriteImage(im_2, "Data_cropped\\ear1-0"+ i +".mha")
+        elif len(i) == 3:
+            sitk.WriteImage(im_1, "Data_cropped\\ear1-"+ i +".mha")
+            sitk.WriteImage(im_2, "Data_cropped\\ear1-"+ i +".mha")
+
+        
+
+        
+
+
 
 
