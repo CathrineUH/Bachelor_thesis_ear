175432bfd782e940e4f52aa3d0599ca6b0d82c8e
TheresaDF <s194329@student.dtu.dk>
2022-03-07 15:07:55 +0100

    updtae job name

--- rl-medical-master/src/batchjob.sh
+++ rl-medical-master/src/batchjob.sh
@@ -15,7 +15,7 @@ mkdir -p "logs/"
 ### –- specify queue --
 #BSUB -q gpuv100
 ### -- set the job Name --
-#BSUB -J UNet
+#BSUB -J RL test 
 ### -- ask for number of cores (default: 1) -- 
 #BSUB -n 1 
 ### -- Select the resources: 1 gpu in exclusive process mode --
