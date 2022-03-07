0bb4945cb658948b1d9ac738cc84d4eec38b6b5c
CathrineUH <97507818+CathrineUH@users.noreply.github.com>
2022-03-07 12:15:59 +0100

    data

--- rl-medical-master/src/dataReader.py
+++ rl-medical-master/src/dataReader.py
@@ -12,8 +12,6 @@ warnings.simplefilter("ignore", category=ResourceWarning)
 
 __all__ = [
     'filesListBrainMRLandmark',
-    'filesListCardioLandmark',
-    'filesListFetalUSLandmark',
     'NiftiImage']
 
 
@@ -28,33 +26,6 @@ def getLandmarksFromTXTFile(file, split=' '):
             landmarks.append([float(k) for k in line.split(split)])
         landmarks = np.asarray(landmarks).reshape((-1, 3))
         return landmarks
-
-
-def getLandmarksFromVTKFile(file):
-    """
-    Extract each landmark point line by line from a VTK file, and return vector
-    containing all landmarks.
-    For cardiac data landmark indexes:
-        0-2 RV insert points
-        1 -> RV lateral wall turning point
-        3 -> LV lateral wall mid-point
-        4 -> apex
-        5-> center of the mitral valve
-    """
-    with open(file) as fp:
-        landmarks = []
-        for i, line in enumerate(fp):
-            if i == 5:
-                landmarks.append([float(k) for k in line.split()])
-            elif i == 6:
-                landmarks.append([float(k) for k in line.split()])
-            elif i > 6:
-                landmarks = np.asarray(landmarks).reshape((-1, 3))
-                # correct landmark according to image direction
-                landmarks[:, [0, 1]] = -landmarks[:, [0, 1]]
-                return landmarks
-
-###############################################################################
 
 
 class filesListBrainMRLandmark(object):
@@ -120,143 +91,6 @@ class filesListBrainMRLandmark(object):
                 # extract filename from path, remove .nii.gz extension
                 image_filenames = [self.image_files[idx][:-7]] * self.agents
                 images = [image] * self.agents
-                yield (images, landmarks, image_filenames,
-                       sitk_image.GetSpacing())
-###############################################################################
-
-class filesListCardioLandmark(object):
-    """ A class for managing train files for mri cardiac data
-        Attributes:
-        files_list: Two or one text files that contain a list of all images and
-        (landmarks)
-        returnLandmarks: Return landmarks if task is train or eval
-        (default: True)
-    """
-
-    def __init__(self, files_list=None, returnLandmarks=True, agents=1):
-        # check if files_list exists
-        assert files_list, 'There is no file given'
-        # read image filenames
-        self.image_files = [line.split('\n')[0]
-                            for line in open(files_list[0].name)]
-        # read landmark filenames if task is train or eval
-        self.returnLandmarks = returnLandmarks
-        self.agents = agents
-        if self.returnLandmarks:
-            self.landmark_files = [
-                line.split('\n')[0] for line in open(
-                    files_list[1].name)]
-            assert len(
-                self.image_files) == len(
-                self.landmark_files), """number of image files is not equal to
-                number of landmark files"""
-
-    @property
-    def num_files(self):
-        return len(self.image_files)
-
-    def sample_circular(self, landmark_ids, shuffle=False):
-        """ return a random sampled ImageRecord from the list of files
-        """
-        if shuffle:
-            # indexes = rng.choice(x, len(x), replace=False)
-            pass
-        else:
-            indexes = np.arange(self.num_files)
-
-        while True:
-            for idx in indexes:
-                sitk_image, image = NiftiImage().decode(self.image_files[idx])
-                if self.returnLandmarks:
-                    landmark_file = self.landmark_files[idx]
-                    all_landmarks = getLandmarksFromVTKFile(landmark_file)
-                    # transform landmarks to image coordinates
-                    all_landmarks = [
-                        sitk_image.TransformPhysicalPointToContinuousIndex(
-                            point) for point in all_landmarks]
-                    # Indexes: 0-2 RV insert points
-                    # 1 -> RV lateral wall turning point
-                    # 3 -> LV lateral wall mid-point,
-                    # 4 -> apex, 5-> center of the mitral valve
-                    landmarks = [np.round(all_landmarks[landmark_ids[i] % 6])
-                                 for i in range(self.agents)]  # Apex + MV
-                    # landmarks = [np.round(all_landmarks[(i + 3) % 6])
-                    #              for i in range(self.agents)]  # LV + Apex
-                    # landmarks = [np.round(all_landmarks[((i + 1) + 3) % 6])
-                    # for i in range(self.agents)] # LV + MV
-                else:
-                    landmarks = None
-
-                # extract filename from path, remove .nii.gz extension
-                image_filenames = [self.image_files[idx][:-7]] * self.agents
-                images = [image] * self.agents
-
-                yield (images, landmarks, image_filenames,
-                       sitk_image.GetSpacing())
-
-###############################################################################
-
-
-class filesListFetalUSLandmark(object):
-    """ A class for managing train files for fetal ultrasound data
-
-        Attributes:
-        files_list: Two or one text files that contain a list of all images and
-        (landmarks)
-        returnLandmarks: Return landmarks if task is train or eval
-        (default: True)
-    """
-
-    def __init__(self, files_list=None, returnLandmarks=True, agents=1):
-        # check if files_list exists
-        assert files_list, 'There is no file given'
-        # read image filenames
-        self.image_files = [line.split('\n')[0]
-                            for line in open(files_list[0].name)]
-        # read landmark filenames if task is train or eval
-        self.returnLandmarks = returnLandmarks
-        self.agents = agents
-        if self.returnLandmarks:
-            self.landmark_files = [
-                line.split('\n')[0] for line in open(
-                    files_list[1].name)]
-            assert len(
-                self.image_files) == len(
-                self.landmark_files), """number of image files is not equal to
-                number of landmark files"""
-
-    @property
-    def num_files(self):
-        return len(self.image_files)
-
-    def sample_circular(self, landmark_ids, shuffle=False):
-        """ return a random sampled ImageRecord from the list of files
-        """
-        if shuffle:
-            # indexes = rng.choice(x, len(x), replace=False)
-            pass
-        else:
-            indexes = np.arange(self.num_files)
-
-        while True:
-            for idx in indexes:
-                sitk_image, image = NiftiImage().decode(self.image_files[idx])
-                if self.returnLandmarks:
-                    landmark_file = self.landmark_files[idx]
-                    all_landmarks = getLandmarksFromTXTFile(
-                        landmark_file, split=' ')
-                    # landmark point 12 csp
-                    # 11 leftCerebellar
-                    # 10 rightCerebellar
-                    landmarks = [np.round(all_landmarks[landmark_ids[i] % 13])
-                                 for i in range(self.agents)]  # Apex + MV
-                else:
-                    landmarks = None
-
-                # extract filename from path, remove .nii.gz extension
-                image_filenames = [self.image_files[idx][:-7]] * self.agents
-                images = [image] * self.agents
-
                 yield (images, landmarks, image_filenames,
                        sitk_image.GetSpacing())
 ###############################################################################
