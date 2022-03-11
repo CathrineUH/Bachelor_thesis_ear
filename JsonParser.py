a42f042a89d9f04823061dcb84f89cd03c89a710
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-11 10:22:36 +0100

    Update JsonParser.py

--- JsonParser.py
+++ JsonParser.py
@@ -5,11 +5,14 @@ from Dataloader import*
 # Tips:
 # https://github.com/InsightSoftwareConsortium/SimpleITK-Notebooks/blob/master/Utilities/intro_animation.py
 # https://stackoverflow.com/questions/30237024/operate-on-slices-of-3d-image-in-simpleitk-and-create-new-3d-image
+
+# https://slicer.readthedocs.io/en/latest/developer_guide/script_repository.html
+# returns JSON object as a dictionary
+
 def read_ear_landmarks(path_json,path_data,full_name):
     f = open(path_json+"\\"+full_name)
     file_name = path_data +"\\"+ full_name[0:12]
-    # https://slicer.readthedocs.io/en/latest/developer_guide/script_repository.html
-    # returns JSON object as a dictionary
+
     data = json.load(f)
     t = data['markups'][0]['controlPoints']
     Dict = {'C': 0, 'A': 1, 'R': 2, 'M': 3, 'T': 4, 'B': 5}
