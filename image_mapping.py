9d3f8fe63bcce6d97f1d59bf5c7ab58a2a298613
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-15 16:54:27 +0100

    Data_Map done on images of dimension 110

--- image_mapping.py
+++ image_mapping.py
@@ -1,4 +1,6 @@
 import numpy as np 
+from Dataloader import getFiles
+import SimpleITK as sitk 
 def image_mapping(image, mu = 230):
     """
     function that given an image maps the pixel values to the range [0, 255]
@@ -9,8 +11,26 @@ def image_mapping(image, mu = 230):
         mapped image with pixel values [0, 255]
     """
     if mu != 230:
-        mu = mu 
-    im_max = np.max(image)
-    im_mapped = (image - mu)* (255/im_max)
-    im_mapped += np.abs(np.min(im_mapped))    
-    return im_mapped 
+        mu = mu
+    
+    im_map = image - mu
+    im_max = np.max(im_map)
+    im_min = np.min(im_map)
+    scaling = 255 / (im_max - im_min)
+    im_map = im_map * scaling 
+    im_map += np.abs(np.min(im_map))    
+    return im_map
+
+
+def map_all_images(path):
+    files = getFiles(path)
+    for f in files:
+        im = (sitk.ReadImage(path + "/" + f))
+        space = im.GetSpacing()
+        orgin = im.GetOrigin()
+        im = sitk.GetArrayFromImage(im)
+        im_map = image_mapping(im)
+        im_map = sitk.GetImageFromArray(im_map)
+        im_map.SetOrigin(orgin)
+        im_map.SetSpacing(space)
+        sitk.WriteImage(im_map, "Data_Map/" + f)
