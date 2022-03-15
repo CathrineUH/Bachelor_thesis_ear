74fdb5a1b53f3b200ab97f2d02ffe596a158219d
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-15 13:37:43 +0100

    Update image_mapping.py

--- image_mapping.py
+++ image_mapping.py
@@ -1,3 +1,4 @@
+import numpy as np 
 def image_mapping(image, mu = 230):
     """
     function that given an image maps the pixel values to the range [0, 255]
@@ -8,9 +9,8 @@ def image_mapping(image, mu = 230):
         mapped image with pixel values [0, 255]
     """
     if mu != 230:
-        print("hello")
         mu = mu 
     im_max = np.max(image)
-    im_mapped = (im_hist - mu)* (255/im_max)
-    im_mapped += np.abs(np.min(im_hist_norm))    
+    im_mapped = (image - mu)* (255/im_max)
+    im_mapped += np.abs(np.min(im_mapped))    
     return im_mapped 
