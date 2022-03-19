7450157d8329d60ace72b722393b5c57bd0649dc
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-19 16:39:19 +0100

    Update computeAngle

--- computeAngle.py
+++ computeAngle.py
@@ -5,42 +5,64 @@ def compute_angle(results, is_degrees=True):
     Function that computes the angle between the facial nerve and chorda tympani
     The calculations are based on the landmarks: C, A, T 
     Input:
-        the results.csv file from evaluation
+        results: the path to the results.csv file from evaluation
         is_degrees: no input for result in degrees. Input for radians 
     Output:
         The angle in radians/degrees 
     """
     if is_degrees != True:
-        is_degrees = False
+        is_degrees = is_degrees
     
     df = pd.read_csv(results, delimiter=',')
     m, _ = df.shape 
     angles_est = np.zeros((m, 1))
     angles_ann = np.zeros((m, 1))
     for i in range(m):
-        # estimated angle 
-        c1, c2, c3 = df.iloc[i, 2:5]
-        a1, a2, a3 = df.iloc[i, 10:13]
-        t1, t2, t3 = df.iloc[i, 34:37]
+        ########################## estimated angle ##########################
+        c1, c2, c3 = df.loc[i, "Agent 0 pos x"], df.loc[i, "Agent 0 pos y"], df.loc[i, "Agent 0 pos z"]
+        a1, a2, a3 = df.loc[i, "Agent 1 pos x"], df.loc[i, "Agent 1 pos y"], df.loc[i, "Agent 1 pos z"]
+        r1, r2, r3 = df.loc[i, "Agent 2 pos x"], df.loc[i, "Agent 2 pos y"], df.loc[i, "Agent 2 pos z"]
+        m1, m2, m3 = df.loc[i, "Agent 3 pos x"], df.loc[i, "Agent 3 pos y"], df.loc[i, "Agent 3 pos z"]
+        t1, t2, t3 = df.loc[i, "Agent 4 pos x"], df.loc[i, "Agent 4 pos y"], df.loc[i, "Agent 4 pos z"]
+
+        # chorda vector 
         v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
-        v2 = np.array([t1 - a1, t2 - a2, t3 - a3])
-        print(v1)
-        print(v2)
+
+        # fit vector for mastoid segment using SVD 
+        data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
+        mu = np.mean(data, axis = 0)
+        _, _, V = np.linalg.svd(data - mu)
+        V = V.T 
+        v2 = V[:, 0]
+
+        # compute angle 
         angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
         if angle >= np.pi:   
             angle = np.pi - angle 
         angles_est[i] = angle 
 
-        # angle found from annotation 
-        c1, c2, c3 = df.iloc[i, 5:8]
-        a1, a2, a3 = df.iloc[i, 13:16]
-        t1, t2, t3 = df.iloc[i, 37:40]
+        ########################## angle found from annotation ##########################
+        c1, c2, c3 = df.loc[i, "Landmark 0 pos x"], df.loc[i, "Landmark 0 pos y"], df.loc[i, "Landmark 0 pos z"]
+        a1, a2, a3 = df.loc[i, "Landmark 1 pos x"], df.loc[i, "Landmark 1 pos y"], df.loc[i, "Landmark 1 pos z"]
+        r1, r2, r3 = df.loc[i, "Landmark 2 pos x"], df.loc[i, "Landmark 2 pos y"], df.loc[i, "Landmark 2 pos z"]
+        m1, m2, m3 = df.loc[i, "Landmark 3 pos x"], df.loc[i, "Landmark 3 pos y"], df.loc[i, "Landmark 3 pos z"]
+        t1, t2, t3 = df.loc[i, "Landmark 4 pos x"], df.loc[i, "Landmark 4 pos y"], df.loc[i, "Landmark 4 pos z"]
+
+        # chorda vector 
         v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
-        v2 = np.array([t1 - a1, t2 - a2, t3 - a3])
+        # fit vector for mastoid segment using SVD 
+        data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
+        mu = np.mean(data, axis = 0)
+        _, _, V = np.linalg.svd(data - mu)
+        V = V.T 
+        v2 = V[:, 0]
+
+        # compute angle 
         angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
         if angle >= np.pi:   
             angle = np.pi - angle 
         angles_ann[i] = angle 
+
     
     if is_degrees:
         angles_est *= 180 / np.pi 
