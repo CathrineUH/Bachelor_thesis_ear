f5aee2fad14a3735a77cd4f115cc372ef7b9920d
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-21 10:09:52 +0100

    Update compute angle

--- computeAngle.py
+++ computeAngle.py
@@ -19,58 +19,10 @@ def compute_angle(results, is_degrees=True):
     angles_ann = np.zeros((m, 1))
     for i in range(m):
         ########################## estimated angle ##########################
-        c1, c2, c3 = df.loc[i, "Agent 0 pos x"], df.loc[i, "Agent 0 pos y"], df.loc[i, "Agent 0 pos z"]
-        a1, a2, a3 = df.loc[i, "Agent 1 pos x"], df.loc[i, "Agent 1 pos y"], df.loc[i, "Agent 1 pos z"]
-        r1, r2, r3 = df.loc[i, "Agent 2 pos x"], df.loc[i, "Agent 2 pos y"], df.loc[i, "Agent 2 pos z"]
-        m1, m2, m3 = df.loc[i, "Agent 3 pos x"], df.loc[i, "Agent 3 pos y"], df.loc[i, "Agent 3 pos z"]
-        t1, t2, t3 = df.loc[i, "Agent 4 pos x"], df.loc[i, "Agent 4 pos y"], df.loc[i, "Agent 4 pos z"]
-
-        # chorda vector 
-        v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
-
-        # fit vector for mastoid segment using SVD 
-        data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
-        mu = np.mean(data, axis = 0)
-        _, _, V = np.linalg.svd(data - mu)
-        V = V.T 
-        v2 = V[:, 0]
-
-        # check for opposite signs 
-        if np.sign(v1[2]) != np.sign(v2[2]):
-            v1 = -v1 
-
-        # compute angle 
-        angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
-        if angle >= np.pi:   
-            angle = np.pi - angle 
-        angles_est[i] = angle 
+        angles_est[i] = get_angle(i, df, "Agent")
 
         ########################## angle found from annotation ##########################
-        c1, c2, c3 = df.loc[i, "Landmark 0 pos x"], df.loc[i, "Landmark 0 pos y"], df.loc[i, "Landmark 0 pos z"]
-        a1, a2, a3 = df.loc[i, "Landmark 1 pos x"], df.loc[i, "Landmark 1 pos y"], df.loc[i, "Landmark 1 pos z"]
-        r1, r2, r3 = df.loc[i, "Landmark 2 pos x"], df.loc[i, "Landmark 2 pos y"], df.loc[i, "Landmark 2 pos z"]
-        m1, m2, m3 = df.loc[i, "Landmark 3 pos x"], df.loc[i, "Landmark 3 pos y"], df.loc[i, "Landmark 3 pos z"]
-        t1, t2, t3 = df.loc[i, "Landmark 4 pos x"], df.loc[i, "Landmark 4 pos y"], df.loc[i, "Landmark 4 pos z"]
-
-        # chorda vector 
-        v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
-        # fit vector for mastoid segment using SVD 
-        data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
-        mu = np.mean(data, axis = 0)
-        _, _, V = np.linalg.svd(data - mu)
-        V = V.T 
-        v2 = V[:, 0]
-
-        #
-        # check for opposite signs 
-        if np.sign(v1[2]) != np.sign(v2[2]):
-            v1 = -v1 
-
-        # compute angle 
-        angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
-        if angle >= np.pi:   
-            angle = np.pi - angle 
-        angles_ann[i] = angle 
+        angles_ann[i] = get_angle(i, df, "Landmark")
 
     
     if is_degrees:
@@ -78,5 +30,31 @@ def compute_angle(results, is_degrees=True):
         angles_ann *= 180 / np.pi 
     return angles_ann, angles_est
 
+def get_angle(i, df, which):
+    c1, c2, c3 = df.loc[i, which + " 0 pos x"], df.loc[i, which + " 0 pos y"], df.loc[i, which + " 0 pos z"]
+    a1, a2, a3 = df.loc[i, which + " 1 pos x"], df.loc[i, which + " 1 pos y"], df.loc[i, which + " 1 pos z"]
+    r1, r2, r3 = df.loc[i, which + " 2 pos x"], df.loc[i, which + " 2 pos y"], df.loc[i, which + " 2 pos z"]
+    m1, m2, m3 = df.loc[i, which + " 3 pos x"], df.loc[i, which + " 3 pos y"], df.loc[i, which + " 3 pos z"]
+    t1, t2, t3 = df.loc[i, which + " 4 pos x"], df.loc[i, which + " 4 pos y"], df.loc[i, which + " 4 pos z"]
+
+    # chorda vector 
+    v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
+
+    # fit vector for mastoid segment using SVD 
+    data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
+    mu = np.mean(data, axis = 0)
+    _, _, V = np.linalg.svd(data - mu)
+    V = V.T 
+    v2 = V[:, 0]
+
+    # check for opposite signs 
+    if np.sign(v1[2]) != np.sign(v2[2]):
+        v1 = -v1 
+
+    # compute angle 
+    angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
+    if angle >= np.pi:   
+        angle = np.pi - angle 
+    return angle 
     
     
