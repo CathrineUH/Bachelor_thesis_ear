4b81f9452ae083650a45207ef7e1672a93c3f614
CathrineUH <97507818+CathrineUH@users.noreply.github.com>
2022-03-21 15:30:10 +0100

    metric

--- metrics.py
+++ metrics.py
@@ -1,22 +1,13 @@
 import numpy as np
 import pandas as pd 
 
-def RMSE(df, number_of_landmarks):
-    rmse = np.zeros((number_of_landmarks,1))
-    m, _ = df.shape 
-    for i in range(number_of_landmarks):
-        temp_sum = 0
-        for j in range(m):
-             temp_sum += ((df.loc[j,"Distance "+ str(i)])**2)
-        rmse[i] = np.sqrt(temp_sum / m)
-    return rmse
 
-def compute_angle(df, is_degrees=True):
+def compute_angle(df, is_degrees = True):
     """
     Function that computes the angle between the facial nerve and chorda tympani
     The calculations are based on the landmarks: C, A, T 
     Input:
-        results: the path to the results.csv file from evaluation
+        df: dataframe of the results
         is_degrees: no input for result in degrees. Input for radians 
     Output:
         The angle in radians/degrees 
@@ -66,24 +57,65 @@ def get_angle(i, df, which):
     if angle >= np.pi:   
         angle = np.pi - angle 
     return angle 
-    
 
-def max_distance(df, number_of_landmarks):
-    max_dis = np.zeros((number_of_landmarks,1))
-    for i in range(number_of_landmarks):
+def RMSE(df, number_of_agents):
+    """
+    Function that computes the Root mean square error.
+    Input:
+        df: dataframe of the results
+        number_of_agents: the number of agents
+    Output:8
+        The root mean sqaure error (mm)
+    """
+    rmse = np.zeros((number_of_agents,1))
+    m, _ = df.shape 
+    for i in range(number_of_agents):
+        temp_sum = 0
+        for j in range(m):
+             temp_sum += ((df.loc[j,"Distance "+ str(i)])**2)
+        rmse[i] = np.sqrt(temp_sum / m)
+    return rmse
+
+def max_distance(df, number_of_agents):
+    """
+    Function that computes the maximum distance of each agent
+    Input:
+        df: dataframe of the results
+        number_of_agents: the number of agents
+    Output:8
+        The maximum distance of each agent (mm)
+    """
+    max_dis = np.zeros((number_of_agents,1))
+    for i in range(number_of_agents):
       max_dis[i] = max(df.loc[:,"Distance "+ str(i)])
     return max_dis
 
-def min_distance(df, number_of_landmarks):
-    min_dis = np.zeros((number_of_landmarks,1))
-    for i in range(number_of_landmarks):
+def min_distance(df, number_of_agents):
+    """
+    Function that computes the minimum distance of each agent
+    Input:
+        df: dataframe of the results
+        number_of_agents: the number of agents
+    Output:8
+        The minimum distance of each agent (mm)
+    """
+    min_dis = np.zeros((number_of_agents,1))
+    for i in range(number_of_agents):
       min_dis[i] = min(df.loc[:,"Distance "+ str(i)])
     return min_dis
 
-def performence_metric(results,number_of_landmarks):
+def performence_metric(results,number_of_agents):
+    """
+    Function that computes the performence metrics and the angels
+    Input:
+        results: the path to the results.csv file from evaluation
+        number_of_agents: the number of agents
+    Output:8
+        The performence metrics(mm) and the angels in radians/degrees
+    """
     df = pd.read_csv(results, delimiter=',')
-    min_dis = min_distance(df,number_of_landmarks)
-    max_dis = max_distance(df,number_of_landmarks)
-    rmse = RMSE(df, number_of_landmarks)
+    min_dis = min_distance(df,number_of_agents)
+    max_dis = max_distance(df,number_of_agents)
+    rmse = RMSE(df, number_of_agents)
     angles_ann, angles_est = compute_angle(df)
     return min_dis,max_dis,rmse, angles_ann,angles_est
