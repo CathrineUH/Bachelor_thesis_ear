f199b46c7c6d2cdaae7fa49cbe6a5f15fe964b70
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-24 12:58:42 +0100

    Update Compute angle

--- computeAngle.py
+++ computeAngle.py
@@ -2,111 +2,111 @@ import numpy as np
 import pandas as pd 
 import json 
 import SimpleITK as sitk 
-# def compute_angle_pca(results, is_degrees=True):
-#     """
-#     Function that computes the angle between the facial nerve and chorda tympani
-#     The calculations are based on the landmarks: C, A, and R, M, T  
-#     Input:
-#         results: the path to the results.csv file from evaluation
-#         is_degrees: no input for result in degrees. Input for radians 
-#     Output:
-#         The angle in radians/degrees 
-#     """
-#     if is_degrees != True:
-#         is_degrees = is_degrees
+def compute_angle_index_pca(results, is_degrees=True):
+    """
+    Function that computes the angle between the facial nerve and chorda tympani
+    The calculations are based on the landmarks: C, A, and R, M, T  
+    Input:
+        results: the path to the results.csv file from evaluation
+        is_degrees: no input for result in degrees. Input for radians 
+    Output:
+        The angle in radians/degrees 
+    """
+    if is_degrees != True:
+        is_degrees = is_degrees
     
-#     df = pd.read_csv(results, delimiter=',')
-#     m, _ = df.shape 
-#     angles_est = np.zeros((m, 1))
-#     angles_ann = np.zeros((m, 1))
-#     for i in range(m):
-#         ########################## estimated angle ##########################
-#         angles_est[i] = get_angle_pca(i, df, "Agent")
+    df = pd.read_csv(results, delimiter=',')
+    m, _ = df.shape 
+    angles_est = np.zeros((m, 1))
+    angles_ann = np.zeros((m, 1))
+    for i in range(m):
+        ########################## estimated angle ##########################
+        angles_est[i] = get_angle_index_pca(i, df, "Agent")
 
-#         ########################## angle found from annotation ##########################
-#         angles_ann[i] = get_angle_pca(i, df, "Landmark")
+        ########################## angle found from annotation ##########################
+        angles_ann[i] = get_angle_index_pca(i, df, "Landmark")
 
     
-#     if is_degrees:
-#         angles_est *= 180 / np.pi 
-#         angles_ann *= 180 / np.pi 
-#     return angles_ann, angles_est
+    if is_degrees:
+        angles_est *= 180 / np.pi 
+        angles_ann *= 180 / np.pi 
+    return angles_ann, angles_est
 
-# def get_angle_pca(i, df, which):
-#     c1, c2, c3 = df.loc[i, which + " 0 pos x"], df.loc[i, which + " 0 pos y"], df.loc[i, which + " 0 pos z"]
-#     a1, a2, a3 = df.loc[i, which + " 1 pos x"], df.loc[i, which + " 1 pos y"], df.loc[i, which + " 1 pos z"]
-#     r1, r2, r3 = df.loc[i, which + " 2 pos x"], df.loc[i, which + " 2 pos y"], df.loc[i, which + " 2 pos z"]
-#     m1, m2, m3 = df.loc[i, which + " 3 pos x"], df.loc[i, which + " 3 pos y"], df.loc[i, which + " 3 pos z"]
-#     t1, t2, t3 = df.loc[i, which + " 4 pos x"], df.loc[i, which + " 4 pos y"], df.loc[i, which + " 4 pos z"]
+def get_angle_index_pca(i, df, which):
+    c1, c2, c3 = df.loc[i, which + " 0 pos x"], df.loc[i, which + " 0 pos y"], df.loc[i, which + " 0 pos z"]
+    a1, a2, a3 = df.loc[i, which + " 1 pos x"], df.loc[i, which + " 1 pos y"], df.loc[i, which + " 1 pos z"]
+    r1, r2, r3 = df.loc[i, which + " 2 pos x"], df.loc[i, which + " 2 pos y"], df.loc[i, which + " 2 pos z"]
+    m1, m2, m3 = df.loc[i, which + " 3 pos x"], df.loc[i, which + " 3 pos y"], df.loc[i, which + " 3 pos z"]
+    t1, t2, t3 = df.loc[i, which + " 4 pos x"], df.loc[i, which + " 4 pos y"], df.loc[i, which + " 4 pos z"]
 
-#     # chorda vector 
-#     v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
+    # chorda vector 
+    v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
 
-#     # fit vector for mastoid segment using SVD 
-#     data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
-#     mu = np.mean(data, axis = 0)
-#     _, _, V = np.linalg.svd(data - mu)
-#     V = V.T 
-#     v2 = V[:, 0]
+    # fit vector for mastoid segment using SVD 
+    data = np.array([[r1, r2, r3], [m1, m2, m3], [t1, t2, t3]])
+    mu = np.mean(data, axis = 0)
+    _, _, V = np.linalg.svd(data - mu)
+    V = V.T 
+    v2 = V[:, 0]
 
-#     # check for opposite signs 
-#     if np.sign(v1[2]) != np.sign(v2[2]):
-#         v1 = -v1 
+    # check for opposite signs 
+    if np.sign(v1[2]) != np.sign(v2[2]):
+        v1 = -v1 
 
-#     # compute angle 
-#     angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
-#     if angle >= np.pi:   
-#         angle = np.pi - angle 
-#     return angle 
+    # compute angle 
+    angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
+    if angle >= np.pi:   
+        angle = np.pi - angle 
+    return angle 
     
 
-# def compute_angle_naiv(results, is_degrees = True):
-#     """
-#     Function that computes the angle between the facial nerve and chorda tympani
-#     The calculations are based on the landmarks: C, A, T 
-#     Input:
-#         results: the path to the results.csv file from evaluation
-#         is_degrees: no input for result in degrees. Input for radians 
-#     Output:
-#         The angle in radians/degrees 
-#     """
-#     if is_degrees != True:
-#         is_degrees = is_degrees
+def compute_angle_index_naiv(results, is_degrees = True):
+    """
+    Function that computes the angle between the facial nerve and chorda tympani
+    The calculations are based on the landmarks: C, A, T 
+    Input:
+        results: the path to the results.csv file from evaluation
+        is_degrees: no input for result in degrees. Input for radians 
+    Output:
+        The angle in radians/degrees 
+    """
+    if is_degrees != True:
+        is_degrees = is_degrees
     
-#     df = pd.read_csv(results, delimiter=',')
-#     m, _ = df.shape 
-#     angles_est = np.zeros((m, 1))
-#     angles_ann = np.zeros((m, 1))
+    df = pd.read_csv(results, delimiter=',')
+    m, _ = df.shape 
+    angles_est = np.zeros((m, 1))
+    angles_ann = np.zeros((m, 1))
 
-#     for i in range(m):
-#         ########################## estimated angle ##########################
-#         angles_est[i] = get_angle_naiv(i, df, "Agent")
+    for i in range(m):
+        ########################## estimated angle ##########################
+        angles_est[i] = get_angle_index_naiv(i, df, "Agent")
 
-#         ########################## angle found from annotation ##########################
-#         angles_ann[i] = get_angle_naiv(i, df, "Landmark")
+        ########################## angle found from annotation ##########################
+        angles_ann[i] = get_angle_index_naiv(i, df, "Landmark")
     
-#     if is_degrees:
-#         angles_est *= 180 / np.pi 
-#         angles_ann *= 180 / np.pi 
-#     return angles_ann, angles_est
+    if is_degrees:
+        angles_est *= 180 / np.pi 
+        angles_ann *= 180 / np.pi 
+    return angles_ann, angles_est
         
 
 
 
-# def get_angle_naiv(i, df, which):
-#     c1, c2, c3 = df.loc[i, which + " 0 pos x"], df.loc[i, which + " 0 pos y"], df.loc[i, which + " 0 pos z"]
-#     a1, a2, a3 = df.loc[i, which + " 1 pos x"], df.loc[i, which + " 1 pos y"], df.loc[i, which + " 1 pos z"]
-#     t1, t2, t3 = df.loc[i, which + " 4 pos x"], df.loc[i, which + " 4 pos y"], df.loc[i, which + " 4 pos z"]
+def get_angle_index_naiv(i, df, which):
+    c1, c2, c3 = df.loc[i, which + " 0 pos x"], df.loc[i, which + " 0 pos y"], df.loc[i, which + " 0 pos z"]
+    a1, a2, a3 = df.loc[i, which + " 1 pos x"], df.loc[i, which + " 1 pos y"], df.loc[i, which + " 1 pos z"]
+    t1, t2, t3 = df.loc[i, which + " 4 pos x"], df.loc[i, which + " 4 pos y"], df.loc[i, which + " 4 pos z"]
 
-#     # find vectors 
-#     v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
-#     v2 = np.array([t1 - a1, t2 - a2, t3 - a3])
+    # find vectors 
+    v1 = np.array([c1 - a1, c2 - a2, c3 - a3])
+    v2 = np.array([t1 - a1, t2 - a2, t3 - a3])
 
-#     # compute angle 
-#     angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
-#     if angle >= np.pi:   
-#         angle = np.pi - angle 
-#     return angle 
+    # compute angle 
+    angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1) * np.linalg.norm(v2)))
+    if angle >= np.pi:   
+        angle = np.pi - angle 
+    return angle 
 
 def read_json_files():
     """
@@ -216,7 +216,7 @@ def get_physical_point_from_result(df, number_of_agents):
 
 
 
-def compute_angle_naiv(df, number_of_agents, is_degrees=True):
+def compute_angle_physical_naiv(df, number_of_agents, is_degrees=True):
 
     if is_degrees != True:
         is_degrees = is_degrees
@@ -269,7 +269,7 @@ def get_pca_direction(R, M, T):
     return direction 
 
 
-def compute_angle_pca(df, number_of_agents, is_degrees=True):
+def compute_angle_physical_pca(df, number_of_agents, is_degrees=True):
     if is_degrees != True:
         is_degrees = is_degrees
     
