32a478227c5b18b8b32ba126bd068d048185fd50
TheresaDF <97507841+TheresaDF@users.noreply.github.com>
2022-03-24 15:09:43 +0100

    Update metrics

--- latex_functions.py
+++ latex_functions.py
@@ -1,29 +1,34 @@
 def print_errors_latex(error):
     m, _ = error.shape
     # min
+    print("\\textbf{Min AE} [mm] &", end = "")
     for i in range(m):
         if i == m - 1:
             print("$" + str(round(error[i][0], 2)) + "$" + "\\\\", end = "")
         else: 
             print("$" + str(round(error[i][0], 2)) + "$" + " &", end = "")
     print("\n")
+
     # max
+    print("\\textbf{Max AE} [mm] &", end = "")
     for i in range(m):
         if i == m - 1:
             print("$" + str(round(error[i][1], 2)) + "$" + "\\\\", end = "")
         else: 
             print("$" + str(round(error[i][1], 2)) + "$" + " &", end = "")
-
     print("\n")
+
     # RMSE 
+    print("\\textbf{RMSE} [mm] &", end = "")
     for i in range(m):
         if i == m - 1:
             print("$" + str(round(error[i][2], 2)) + "$" + "\\\\", end = "")
         else: 
             print("$" + str(round(error[i][2], 2)) + "$" + " &", end = "")
-        
     print("\n")
+
     # estimation error 
+    print("\\textbf{EE} [mm] &", end = "")
     for i in range(m):
         if i == m - 1:
             print("$" + str(round(error[i][3], 2)) + " \\pm " + str(round(error[i][4], 2)) + "$" + "\\\\", end = "")
