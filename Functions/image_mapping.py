6a555b7f47eca3ef7ed371a9bba8b8e41078a265
CathrineUH <97507818+CathrineUH@users.noreply.github.com>
2022-04-15 21:31:08 +0200

    ccc

--- Functions/image_mapping.py
+++ Functions/image_mapping.py
@@ -2,10 +2,10 @@ import numpy as np
 from .Dataloader import getFiles
 import SimpleITK as sitk 
 
-def compute_global_max(files, quantile):
+def compute_global_max(image_path,files, quantile):
     all_images = np.zeros((96, 110, 110, 110))
     for count, i in enumerate(files):
-        im = sitk.GetArrayFromImage(sitk.ReadImage("junk/Data_use_100/" + i))
+        im = sitk.GetArrayFromImage(sitk.ReadImage(image_path+"/" + i))
         all_images[count] = im 
     global_max = np.quantile(all_images, quantile)
     return global_max 
@@ -16,17 +16,17 @@ def scale_image(im, global_max):
     return im  
 
 
-def save_all_scaled_images(quantile):
+def save_all_scaled_images(image_path,quantile):
     # im_ref_idx = 15 
 
-    files = getFiles("junk/Data_use_100")
+    files = getFiles(image_path)
     global_max = compute_global_max(files, quantile)
 
     for count, f in enumerate(files):
         if count == 32: 
             continue 
         # load 
-        im = sitk.ReadImage("junk/Data_use_100/" + f)
+        im = sitk.ReadImage(image_path+"/" + f)
         space = im.GetSpacing()
         orgin = im.GetOrigin()
         im = sitk.GetArrayFromImage(im)
@@ -38,7 +38,7 @@ def save_all_scaled_images(quantile):
         im_scaled = sitk.GetImageFromArray(im_scaled)
         im_scaled.SetOrigin(orgin)
         im_scaled.SetSpacing(space)
-        sitk.WriteImage(im_scaled, "Data_Scaled/" + f)
+        sitk.WriteImage(im_scaled, "Data_good_Scaled/" + f)
 
 
 
