d1587d62270c1574323ae4e2c4fe7ef50cb532e5
CathrineUH <97507818+CathrineUH@users.noreply.github.com>
2022-05-10 11:07:18 +0200

    jj

--- Functions/Dijkstras.py
+++ Functions/Dijkstras.py
@@ -202,7 +202,7 @@ class Dijkstras:
         facial_plot = facial * l + mu 
         return facial_plot
 
-    def intersection(self, chorda, facial):
+    def intersection(self, chorda, facial,angle,number):
         # defining points of lines 
         x1, y1, _ = chorda[0]
         x2, y2, _= chorda[-1]
@@ -216,7 +216,94 @@ class Dijkstras:
         # points of intersection 
         Px = (first * (x3 - x4) - (x1 - x2) * last)/ D
         Py = (first * (y3 - y4) - (y1 - y2) * last) / D
-        return Px, Py
+        r = 2 
+
+        x_list = []
+        y_list = []
+        slope_facial = (y4 - y3) / (x4 - x3)
+        slope_chorda = (y2 - y1) / (x2 - x1)
+        b_facial = y4 - slope_facial * x4 
+        b_chorda = y1 - slope_chorda * x1 
+        def line_circle_intersect(m, b, x0, y0, r): 
+            c1 = 1 + m ** 2
+            c2 = - 2.0 * x0 + 2 * m * ( b - y0 )
+            c3 = x0 * 2 + ( b - y0 ) * 2 - r ** 2
+
+            # solve the quadratic equation:
+            delta = c2 ** 2 - 4.0 * c1 * c3
+
+            x1 = ( - c2 + np.sqrt(delta) ) / ( 2.0 * c1 )
+            x2 = ( - c2 - np.sqrt(delta) ) / ( 2.0 * c1 )
+
+            x_list.append(x1)
+            x_list.append(x2)
+
+            y1 = m * x1 + b
+            y2 = m * x2 + b
+
+            y_list.append(y1)
+            y_list.append(y2)
+                
+        line_circle_intersect(slope_facial, b_facial, Px, Py, r)
+        line_circle_intersect(slope_chorda, b_chorda, Px, Py, r)
+
+        def get_point_angle(x,y,x0,y0):
+
+            num = x - x0
+            den = np.sqrt( ( x - x0 )**2 + ( y - y0 )**2 )
+
+            theta = np.arccos( num / den )
+
+            if not y - y0 >= 0: theta = 2 * np.pi - theta
+
+            #print(theta, np.rad2deg(theta), y - y0 )
+
+            return theta
+
+        theta_list = []
+
+        for i in range(len(x_list)):
+
+            x = x_list[i]
+            y = y_list[i]
+
+            theta_list.append(get_point_angle(x,y,Px,Py) )
+        
+        P0 = theta_list[0]
+        P1 = theta_list[1]
+        P2 = theta_list[2]
+        P3 = theta_list[3]
+       
+        if(abs(P1-P3)*180/np.pi > angle-0.1 and abs(P1-P3)*180/np.pi<angle+0.1):
+            theta1 = P1 
+            theta2 = P3
+
+        elif((abs(P0-P2)*180/np.pi > angle-0.1 and abs(P0-P2)*180/np.pi<angle+0.1)):
+            theta1 = P0
+            theta2 = P2
+
+        elif((abs(P0-P3)*180/np.pi > angle-0.1 and abs(P0-P3)*180/np.pi < angle+0.1)):
+            theta1 = P0
+            theta2 = P3
+        elif((abs(P2-P3)*180/np.pi > angle-0.1 and abs(P2-P3)*180/np.pi<angle+0.1)):
+            theta1 = P2
+            theta2 = P3
+        elif((abs(P0-P1)*180/np.pi > angle-0.1 and abs(P0-P1)*180/np.pi<angle+0.1)):
+            theta1 = P0
+            theta2 = P1
+        else:
+            theta1 = P1
+            theta2 = P2
+
+            
+        circ = np.linspace(theta1, theta2, 100)
+        circ_x = r * np.cos(circ) + Px 
+        circ_y = r * np.sin(circ) + Py 
+
+        mid_angle = (theta1 + theta2) / 2.0 
+        mid_angle_x = (r + number) * np.cos(mid_angle) + Px -1
+        mid_angle_y = (r + number) * np.sin(mid_angle) + Py
+        return circ_x, circ_y, mid_angle_x, mid_angle_y, Px,Py
 
     def drilling_point(self, angle, c, px, py):
         Hoz_point = np.array([[1],[0]])
@@ -292,15 +379,16 @@ class Dijkstras:
 
     def plotDijkstras(self,chorda_point, facial_point, chorda, facial, Dril_x, Dril_y, angle,title):
         mat.rcParams.update({'font.size': 18})
-        plt.figure()
-        plt.scatter(chorda_point[:, 0], chorda_point[:, 1], color = col(8).color) 
-        plt.scatter(facial_point[:, 0], facial_point[:, 1], color = col(1).color)
+        plt.figure(figsize=(8,6))
+        plt.scatter(chorda_point[:, 0], chorda_point[:, 1], marker = "*", color = [0.63215686, 0.39607843, 0.68980392], s = 50) 
+        plt.scatter(facial_point[:, 0], facial_point[:, 1],marker = "o", color =[0.3 , 0.67333333, 0.4454902],s = 50)
         plt.axis("square")
-        plt.plot(chorda[:, 0], chorda[:, 1], linestyle = '-', color = col(10).color, label = "CTY")
-        plt.plot(facial[:, 0], facial[:, 1], linestyle = '-', color = col(9).color, label = "FN")
+        plt.plot(chorda[:, 0], chorda[:, 1], linestyle = '-', color = [0.63215686, 0.39607843, 0.68980392], label = "CTY",linewidth = 5*0.4)
+        plt.plot(facial[:, 0], facial[:, 1], linestyle = '-', color = [0.3 , 0.67333333, 0.4454902], label = "FN",linewidth = 5*1.2)
         plt.scatter(Dril_x, Dril_y, marker = "x", color = col(0).color, label = "DP")
         plt.legend()
         plt.title(title + ": " + "Angle = " + str(angle))
+        # plt.title("Shortest Path")
         plt.xticks([])
         plt.yticks([])
         min_x = np.min(np.concatenate([facial[:, 0],chorda[:, 0]], axis = 0))
@@ -333,8 +421,8 @@ class Dijkstras:
         Px_ann, Py_ann = self.intersection(chorda_ann, facial_ann)
         Dril_x_model, Dril_Y_model = self.drilling_point(angle_model,chorda_model,Px_model,Py_model)
         Dril_x_ann, Dril_Y_ann = self.drilling_point(angle_ann,chorda_ann,Px_ann,Py_ann)
-        Viz.plot_angle_ann(self.nr_model,Dril_x_ann = Dril_x_model, Dril_y_ann = Dril_Y_model)
-        chorda_model, facial_model = self.plotDijkstras(chorda_point_model, facial_point_model, chorda_model, facial_model, Dril_x_ann, Dril_Y_ann ,np.round(angle_model, 2),"Model")
+        # Viz.plot_angle_ann(self.nr_model,Dril_x_ann = Dril_x_model, Dril_y_ann = Dril_Y_model)
+        # chorda_model, facial_model = self.plotDijkstras(chorda_point_model, facial_point_model, chorda_model, facial_model, Dril_x_ann, Dril_Y_ann ,np.round(angle_model, 2),"Model")
         chorda_ann, facial_ann = self.plotDijkstras(chorda_point_ann, facial_point_ann, chorda_ann, facial_ann, Dril_x_ann, Dril_Y_ann,np.round(angle_ann, 2),"Landmark")
     
     def compute_all_angles(self): 
@@ -351,6 +439,61 @@ class Dijkstras:
             angles[nr, 0], angles[nr, 1] = angle_ann, angle_model 
         
         return angles 
+    def plot_together(self):
+        chorda_point_model, chorda_point_ann,facial_point_model, facial_point_ann, angle_ann, angle_model, facial_model,facial_ann, chorda_model,chorda_ann = self.visualize()
+        circ_x_model, circ_y_model, mid_angle_x_model, mid_angle_y_model,Px_model, Py_model = self.intersection(chorda_model, facial_model,angle_model,2.8)
+        circ_x_ann, circ_y_ann, mid_angle_x_ann, mid_angle_y_ann,Px_ann, Py_ann = self.intersection(chorda_ann, facial_ann,angle_ann, 2.8)
+        Dril_x_model, Dril_Y_model = self.drilling_point(angle_model,chorda_model,Px_model,Py_model)
+        Dril_x_ann, Dril_Y_ann = self.drilling_point(angle_ann,chorda_ann,Px_ann,Py_ann)
+        mat.rcParams.update({'font.size': 18})
+        plt.figure(figsize=(8,6))
+        plt.plot(chorda_model[:, 0], chorda_model[:, 1], linestyle = '-', color = [0.3 , 0.67333333, 0.4454902], label = "Model", alpha = 0.7, linewidth = 5*0.4, zorder=0)
+        plt.plot(facial_model[:, 0], facial_model[:, 1], linestyle = '-', color = [0.3 , 0.67333333, 0.4454902], alpha = 0.7,linewidth = 5*1.2,zorder=0)
 
+        plt.plot(chorda_ann[:, 0], chorda_ann[:, 1], linestyle = '-', color = [0.63215686, 0.39607843, 0.68980392], label = "Ann", alpha = 0.7,linewidth = 5*0.4,zorder=0)
+        plt.plot(facial_ann[:, 0], facial_ann[:, 1], linestyle = '-', color = [0.63215686, 0.39607843, 0.68980392], alpha = 0.7,linewidth = 5*1.2,zorder=0)
+
+        
+        # plt.scatter(chorda_point_model[:, 0],  chorda_point_model[:, 1], marker = "o", color =[0.3 , 0.67333333, 0.4454902], s=50,zorder=5)
+        # plt.scatter(facial_point_model[:, 0],  facial_point_model[:, 1], marker = "o", color =[0.3 , 0.67333333, 0.4454902], s=50,zorder=5)
+        # plt.scatter(chorda_point_ann[:, 0], chorda_point_ann[:, 1], marker = "*", color = [0.63215686, 0.39607843, 0.68980392], s=50,zorder=5 )
+        # plt.scatter(facial_point_ann[:, 0], facial_point_ann[:, 1], marker = "*", color = [0.63215686, 0.39607843, 0.68980392], s=50,zorder=5 )
+
+        plt.plot(circ_x_model, circ_y_model, color = [0.3 , 0.67333333, 0.4454902])
+        plt.text(51,52, np.round(angle_model,2), fontsize = 16)
+        plt.plot(circ_x_ann, circ_y_ann, color = [0.63215686, 0.39607843, 0.68980392])
+        plt.text(mid_angle_x_ann,mid_angle_y_ann, np.round(angle_ann,2), fontsize = 16)
+        plt.scatter(Dril_x_model, Dril_Y_model, marker = "x", color = col(0).color, label = "Model", s = 50, zorder=10)
+        plt.scatter(Dril_x_ann, Dril_Y_ann, marker = "x", color = col(3).color, label = "Ann",s = 50, zorder=10)
+
+        plt.legend(loc = 'lower left')
+        plt.axis("square")
+        plt.gca().set_aspect("equal")
+        # plt.title(title)
+        min_x = np.min(np.concatenate([chorda_model[:, 0],facial_model[:, 0],facial_ann[:, 0],chorda_ann[:, 0]], axis = 0))
+        min_x = np.min([min_x,Dril_x_model,Dril_x_ann])
+        min_y = np.min(np.concatenate([chorda_model[:, 1],facial_model[:, 1],facial_ann[:, 1],chorda_ann[:, 1]], axis = 0))
+        min_y = np.min([min_y,Dril_Y_model,Dril_Y_ann])
+        max_x = np.max(np.concatenate([chorda_model[:, 0],facial_model[:, 0],facial_ann[:, 0],chorda_ann[:, 0]], axis = 0))
+        max_x = np.max([max_x,Dril_x_model,Dril_x_ann])
+        max_y = np.max(np.concatenate([chorda_model[:, 1],facial_model[:, 1],facial_ann[:, 1],chorda_ann[:, 1]], axis = 0))
+        max_y = np.max([max_y,Dril_Y_model,Dril_Y_ann])
+
+        x_lim = max_x-min_x
+        y_lim = max_y-min_y
+        if(x_lim> y_lim):
+            val = (x_lim-y_lim)/2
+            max_y = max_y +val
+            min_y = min_y -val
+        else:
+            val = (y_lim-x_lim)/2
+            max_x = max_x +val
+            min_x = min_x -val
+        plt.title("On Nerve")
+        plt.xlim([min_x - 2, max_x + 2])
+        plt.ylim([min_y - 2, max_y + 2])
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
     
-    
+      
